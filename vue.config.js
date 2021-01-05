c1a4c0fd4f88ace68bbe9128ec67c2e578d3a957
GRANDSOFT\yangb-i <yangb-i@glodon.com>
2021-01-05 20:09:05 +0800

    打包压缩配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
+const CompressionPlugin = require("compression-webpack-plugin")
+
+// https://www.webpackjs.com/plugins/compression-webpack-plugin/#%E9%80%89%E9%A1%B9
+
 module.exports = {
   publicPath: './',
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      new CompressionPlugin({
+        algorithm: 'gzip',
+        test: /\.js$|\.html$|\.css/, // 匹配文件名
+        threshold: 1024*5, // 文件压缩阈值，对超过10 k的进行压缩
+        deleteOriginalAssets: true, // 是否删除源文件
+        minRatio: 0.8
+      })
+    ]
+  }
 }
